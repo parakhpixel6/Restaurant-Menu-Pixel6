761b0b597a36e8978ae7897d59ff3089a11983e9
parakh.pixel6 <parakh@pixel6.co>
2023-06-09 11:52:09 +0530

    Sidebar Final JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,55 @@
+//SIDEBAR NAVIGATIONS SUBSUBMENU LINK COLOR SWITCHING
+const navigationSubMenuLinks = document.querySelectorAll('.sidebar--navigations-subsubmenu');
 
-//SIDEBAR NAVIGATIONS LINK COLOR SWITCHING
+// Loop through the navigation links and add a click event listener
+navigationSubMenuLinks.forEach(subMenuLink => {
+  subMenuLink.addEventListener('click', function() {
+    navigationSubMenuLinks.forEach(subMenuLink => {
+        subMenuLink.classList.remove('active');
+    });
+    this.classList.add('active');
+  });
+});
+
+// Get all sidebar--navigations-submenu elements
+const subMenus = document.querySelectorAll('.sidebar--navigations-submenu');
+
+// Add click event listener to each submenu
+subMenus.forEach(subMenu => {
+    const link = subMenu.querySelector('.sidebar--navigations-link');
+    const subSubMenu = subMenu.querySelector('.sidebar--navigations-submenu-link');
+    const subsubMenuActive = subMenu.querySelector('.sidebar--navigations-subsubmenu');
+
+    // Toggle active class and display on click
+    link.addEventListener('click', () => {
+        subMenus.forEach(otherSubMenu => {
+            if (otherSubMenu !== subMenu) {
+                const otherSubSubMenu = otherSubMenu.querySelector('.sidebar--navigations-submenu-link');
+                const otherSubsubMenuActive = otherSubMenu.querySelector('.sidebar--navigations-subsubmenu');
+
+                otherSubMenu.classList.remove('active');
+                otherSubSubMenu.style.display = 'none';
+                otherSubSubMenu.classList.remove('active');
+                otherSubsubMenuActive.classList.remove('active');
+            }
+        });
+
+        subMenu.classList.toggle('active');
+
+        if (subMenu.classList.contains('active')) {
+            subSubMenu.style.display = 'block';
+            subSubMenu.classList.add('active');
+            subsubMenuActive.classList.add('active');
+        } else {
+            subSubMenu.style.display = 'none';
+            subSubMenu.classList.remove('active');
+            subsubMenuActive.classList.remove('active');
+            link.classList.remove('active');
+        }
+    });
+});
+
+// SIDEBAR NAVIGATIONS LINK COLOR SWITCHING
 const navigationLinks = document.querySelectorAll('.sidebar--navigations-link');
 
 // Loop through the navigation links and add a click event listener
@@ -7,46 +57,33 @@ navigationLinks.forEach(link => {
   link.addEventListener('click', function() {
     // Remove active class from all navigation links
     navigationLinks.forEach(link => {
-        const submenuLinkRemove = this.nextElementSibling;
-        link.classList.remove('active');
-
-        if (submenuLinkRemove && submenuLinkRemove.classList.contains('sidebar--navigations-submenu-link')) {
-        // Toggle the display of the submenu link
-            submenuLinkRemove.style.display = 'none';
-        }
+      link.classList.remove('active');
     });
-
-    // Add active class to the clicked navigation link
     this.classList.add('active');
 
-    // Get the submenu link of the clicked navigation link
-    const submenuLink = this.nextElementSibling;
-    if (submenuLink && submenuLink.classList.contains('sidebar--navigations-submenu-link')) {
-    // Toggle the display of the submenu link
-        submenuLink.style.display = 'block';
-    }
+    // Check if the link has a parent with class .sidebar--navigations-submenu
+    const submenuParent = this.closest('.sidebar--navigations-submenu');
+    if (!submenuParent || !submenuParent.classList.contains('active')) {
+      const subMenus = document.querySelectorAll('.sidebar--navigations-submenu');
+      subMenus.forEach(subMenu => {
+        subMenu.classList.remove('active');
+        const subSubMenu = subMenu.querySelector('.sidebar--navigations-submenu-link');
+        const subsubMenuActive = subMenu.querySelector('.sidebar--navigations-subsubmenu');
+        if (subSubMenu && subsubMenuActive) {
+          subSubMenu.style.display = 'none';
+          subSubMenu.classList.remove('active');
+          subsubMenuActive.classList.remove('active');
 
+          // Remove active class from child links
+          const childLinks = subMenu.querySelectorAll('.sidebar--navigations-link');
+          childLinks.forEach(childLink => {
+            childLink.classList.remove('active');
+          });
+        }
+      });
+    }
   });
 });
 
-//SIDEBAR DROPDOWN SHOW 
-
-// const submenuLink = document.querySelectorAll('.sidebar--navigations-submenu-link');
-
-// // const checkPreviousEelementActive = submenuLink.previousElementSibling
-
-// submenuLink.forEach(check => {
-//     check.addEventListener('click', function() {
-//         const checkPreviousEelementActive = check.previousElementSibling;
-
-//         if(checkPreviousEelementActive.classList.contains('active')) {
-//             if (submenuLink.style.display === 'none') {
-//                 submenuLink.style.display = 'block';
-//             } else {
-//                 submenuLink.style.display = 'none';
-//             }
-//         }
-//     });
-// });
 
 
