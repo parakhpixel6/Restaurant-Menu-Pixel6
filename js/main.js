2443dba99df1667eda2a16bf56e350e8d4b96941
parakh.pixel6 <parakh@pixel6.co>
2023-06-12 16:59:58 +0530

    Header 1 Welcome msg, time, date

--- js/main.js
+++ js/main.js
@@ -97,25 +97,139 @@ navigationLinks.forEach(link => {
   });
 });
 
-function printTime() {
-  var now = new Date();
-  var hours = now.getHours();
-  var minutes = now.getMinutes();
 
-  // Add leading zeros if necessary
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
+// // SIDEBAR NAVIGATIONS SUBSUBMENU LINK COLOR SWITCHING
+// const navigationSubMenuLinks = document.querySelectorAll('.sidebar--navigations-subsubmenu');
+// let lastVisitedSubSubMenu = null;
 
-  var time = hours + ":" + minutes;
+// // Loop through the navigation links and add a click event listener
+// navigationSubMenuLinks.forEach(subMenuLink => {
+//   subMenuLink.addEventListener('click', function () {
+//     navigationSubMenuLinks.forEach(subMenuLink => {
+//       subMenuLink.classList.remove('active');
+//     });
+//     this.classList.add('active');
+//     lastVisitedSubSubMenu = this;
+//   });
+// });
 
-  //console.log(time); 
-  // Output to the console
-  // You can also update the time dynamically on your webpage
-  // by setting the value of an HTML element using JavaScript
-  // For example:
-  document.getElementById("time").innerHTML = time;
-}
+// // Get all sidebar--navigations-submenu elements
+// const subMenus = document.querySelectorAll('.sidebar--navigations-submenu');
 
+// // Add click event listener to each submenu
+// subMenus.forEach(subMenu => {
+//   const link = subMenu.querySelector('.sidebar--navigations-link');
+//   const subSubMenu = subMenu.querySelector('.sidebar--navigations-submenu-link');
+//   const subsubMenuActive = subMenu.querySelector('.sidebar--navigations-subsubmenu');
+
+//   // Toggle active class and display on click
+//   link.addEventListener('click', () => {
+//     subMenus.forEach(otherSubMenu => {
+//       if (otherSubMenu !== subMenu) {
+//         const otherSubSubMenu = otherSubMenu.querySelector('.sidebar--navigations-submenu-link');
+//         const otherSubsubMenuActive = otherSubMenu.querySelector('.sidebar--navigations-subsubmenu');
+
+//         otherSubMenu.classList.remove('active');
+//         otherSubSubMenu.style.display = 'none';
+//         otherSubSubMenu.classList.remove('active');
+//         otherSubsubMenuActive.classList.remove('active');
+//       }
+//     });
+
+//     subMenu.classList.toggle('active');
+
+//     if (subMenu.classList.contains('active')) {
+//       subSubMenu.style.display = 'block';
+//       //subSubMenu.classList.add('active');
+//       //subsubMenuActive.classList.add('active');
+//       //lastVisitedSubSubMenu = subsubMenuActive;
+//     } else {
+//       subSubMenu.style.display = 'none';
+//       //subSubMenu.classList.remove('active');
+//       //subsubMenuActive.classList.remove('active');
+//       //link.classList.remove('active');
+//       lastVisitedSubSubMenu = null; // Reset lastVisitedSubSubMenu when the submenu is deactivated
+
+
+//     }
+//   });
+// });
+
+// // SIDEBAR NAVIGATIONS LINK COLOR SWITCHING
+// const navigationLinks = document.querySelectorAll('.sidebar--navigations-link');
+
+// // Loop through the navigation links and add a click event listener
+// navigationLinks.forEach(link => {
+//   link.addEventListener('click', function () {
+//     // Remove active class from all navigation links
+//     navigationLinks.forEach(link => {
+//       link.classList.remove('active');
+//     });
+//     this.classList.add('active');
+
+//     // Check if the link has a parent with class .sidebar--navigations-submenu
+//     const submenuParent = this.closest('.sidebar--navigations-submenu');
+//     if (!submenuParent || !submenuParent.classList.contains('active')) {
+//       const subMenus = document.querySelectorAll('.sidebar--navigations-submenu');
+//       subMenus.forEach(subMenu => {
+//         subMenu.classList.remove('active');
+//         const subSubMenu = subMenu.querySelector('.sidebar--navigations-submenu-link');
+//         const subsubMenuActive = subMenu.querySelector('.sidebar--navigations-subsubmenu');
+//         if (subSubMenu && subsubMenuActive) {
+//           subSubMenu.style.display = 'none';
+//           subSubMenu.classList.remove('active');
+//           subsubMenuActive.classList.remove('active');
+
+//           // Remove active class from child links
+//           const childLinks = subMenu.querySelectorAll('.sidebar--navigations-link');
+//           childLinks.forEach(childLink => {
+//             childLink.classList.remove('active');
+//           });
+//         }
+//       });
+//     }
+
+//     // Activate the last visited subsubmenu
+//     if (lastVisitedSubSubMenu) {
+//       lastVisitedSubSubMenu.classList.add('active');
+//       // Remove active class from other subsubmenus
+//       const otherSubSubMenus = document.querySelectorAll('.sidebar--navigations-subsubmenu');
+//       otherSubSubMenus.forEach(otherSubSubMenu => {
+//         if (otherSubSubMenu === lastVisitedSubSubMenu) {
+//           otherSubSubMenu.classList.remove('active');
+//         } 
+//       });
+//     }
+//   });
+// });
+
+
+
+  function printTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+  
+    // Determine whether it is AM or PM
+    var meridiem = hours >= 12 ? "PM" : "AM";
+  
+    // Convert hours to 12-hour format
+    hours = hours % 12;
+    hours = hours ? hours : 12; // Handle midnight (0 hours)
+  
+    // Add leading zeros if necessary
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+  
+    var time = hours + ":" + minutes + " " + meridiem;
+  
+    // Output to the console
+    // You can also update the time dynamically on your webpage
+    // by setting the value of an HTML element using JavaScript
+    // For example:
+    document.getElementById("time").innerHTML = time;
+  }
+  
 // Call the printTime function every second (1000 milliseconds)
 setInterval(printTime, 1000);
 
