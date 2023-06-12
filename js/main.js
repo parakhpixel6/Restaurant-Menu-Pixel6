aeb4a659ed9554f8e10752cf88334668cd719c5c
parakh.pixel6 <parakh@pixel6.co>
2023-06-12 08:15:53 +0530

    js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ navigationSubMenuLinks.forEach(subMenuLink => {
         subMenuLink.classList.remove('active');
     });
     this.classList.add('active');
+    //lastVisitedSubSubMenu = this;
   });
 });
 
@@ -64,26 +65,146 @@ navigationLinks.forEach(link => {
     // Check if the link has a parent with class .sidebar--navigations-submenu
     const submenuParent = this.closest('.sidebar--navigations-submenu');
     if (!submenuParent || !submenuParent.classList.contains('active')) {
-      const subMenus = document.querySelectorAll('.sidebar--navigations-submenu');
-      subMenus.forEach(subMenu => {
+        const subMenus = document.querySelectorAll('.sidebar--navigations-submenu');
+        subMenus.forEach(subMenu => {
         subMenu.classList.remove('active');
         const subSubMenu = subMenu.querySelector('.sidebar--navigations-submenu-link');
         const subsubMenuActive = subMenu.querySelector('.sidebar--navigations-subsubmenu');
         if (subSubMenu && subsubMenuActive) {
-          subSubMenu.style.display = 'none';
-          subSubMenu.classList.remove('active');
-          subsubMenuActive.classList.remove('active');
+            subSubMenu.style.display = 'none';
+            subSubMenu.classList.remove('active');
+            subsubMenuActive.classList.remove('active');
 
-          // Remove active class from child links
-          const childLinks = subMenu.querySelectorAll('.sidebar--navigations-link');
-          childLinks.forEach(childLink => {
-            childLink.classList.remove('active');
-          });
+            // Remove active class from child links
+            const childLinks = subMenu.querySelectorAll('.sidebar--navigations-link');
+            childLinks.forEach(childLink => {
+                childLink.classList.remove('active');
+            });
+
+            if (lastVisitedSubSubMenu) {
+                lastVisitedSubSubMenu.classList.add('active');
+                // Remove active class from other subsubmenus
+                const otherSubSubMenus = document.querySelectorAll('.sidebar--navigations-subsubmenu');
+                otherSubSubMenus.forEach(otherSubSubMenu => {
+                    if (otherSubSubMenu === lastVisitedSubSubMenu) {
+                    otherSubSubMenu.classList.remove('active');
+                    } 
+                });
+            }
         }
       });
     }
   });
 });
+
+// // SIDEBAR NAVIGATIONS SUBSUBMENU LINK COLOR SWITCHING
+// const navigationSubMenuLinks = document.querySelectorAll('.sidebar--navigations-subsubmenu');
+// let lastVisitedSubSubMenu = null;
+
+// // Loop through the navigation links and add a click event listener
+// navigationSubMenuLinks.forEach(subMenuLink => {
+//   subMenuLink.addEventListener('click', function () {
+//     navigationSubMenuLinks.forEach(subMenuLink => {
+//       subMenuLink.classList.remove('active');
+//     });
+//     this.classList.add('active');
+//     lastVisitedSubSubMenu = this;
+//   });
+// });
+
+// // Get all sidebar--navigations-submenu elements
+// const subMenus = document.querySelectorAll('.sidebar--navigations-submenu');
+
+// // Add click event listener to each submenu
+// subMenus.forEach(subMenu => {
+//   const link = subMenu.querySelector('.sidebar--navigations-link');
+//   const subSubMenu = subMenu.querySelector('.sidebar--navigations-submenu-link');
+//   const subsubMenuActive = subMenu.querySelector('.sidebar--navigations-subsubmenu');
+
+//   // Toggle active class and display on click
+//   link.addEventListener('click', () => {
+//     subMenus.forEach(otherSubMenu => {
+//       if (otherSubMenu !== subMenu) {
+//         const otherSubSubMenu = otherSubMenu.querySelector('.sidebar--navigations-submenu-link');
+//         const otherSubsubMenuActive = otherSubMenu.querySelector('.sidebar--navigations-subsubmenu');
+
+//         otherSubMenu.classList.remove('active');
+//         otherSubSubMenu.style.display = 'none';
+//         otherSubSubMenu.classList.remove('active');
+//         otherSubsubMenuActive.classList.remove('active');
+//       }
+//     });
+
+//     subMenu.classList.toggle('active');
+
+//     if (subMenu.classList.contains('active')) {
+//       subSubMenu.style.display = 'block';
+//       //subSubMenu.classList.add('active');
+//       //subsubMenuActive.classList.add('active');
+//       //lastVisitedSubSubMenu = subsubMenuActive;
+//     } else {
+//       subSubMenu.style.display = 'none';
+//       //subSubMenu.classList.remove('active');
+//       //subsubMenuActive.classList.remove('active');
+//       //link.classList.remove('active');
+//       lastVisitedSubSubMenu = null; // Reset lastVisitedSubSubMenu when the submenu is deactivated
+
+
+//     }
+//   });
+// });
+
+// // SIDEBAR NAVIGATIONS LINK COLOR SWITCHING
+// const navigationLinks = document.querySelectorAll('.sidebar--navigations-link');
+
+// // Loop through the navigation links and add a click event listener
+// navigationLinks.forEach(link => {
+//   link.addEventListener('click', function () {
+//     // Remove active class from all navigation links
+//     navigationLinks.forEach(link => {
+//       link.classList.remove('active');
+//     });
+//     this.classList.add('active');
+
+//     // Check if the link has a parent with class .sidebar--navigations-submenu
+//     const submenuParent = this.closest('.sidebar--navigations-submenu');
+//     if (!submenuParent || !submenuParent.classList.contains('active')) {
+//       const subMenus = document.querySelectorAll('.sidebar--navigations-submenu');
+//       subMenus.forEach(subMenu => {
+//         subMenu.classList.remove('active');
+//         const subSubMenu = subMenu.querySelector('.sidebar--navigations-submenu-link');
+//         const subsubMenuActive = subMenu.querySelector('.sidebar--navigations-subsubmenu');
+//         if (subSubMenu && subsubMenuActive) {
+//           subSubMenu.style.display = 'none';
+//           subSubMenu.classList.remove('active');
+//           subsubMenuActive.classList.remove('active');
+
+//           // Remove active class from child links
+//           const childLinks = subMenu.querySelectorAll('.sidebar--navigations-link');
+//           childLinks.forEach(childLink => {
+//             childLink.classList.remove('active');
+//           });
+//         }
+//       });
+//     }
+
+//     // Activate the last visited subsubmenu
+//     if (lastVisitedSubSubMenu) {
+//       lastVisitedSubSubMenu.classList.add('active');
+//       // Remove active class from other subsubmenus
+//       const otherSubSubMenus = document.querySelectorAll('.sidebar--navigations-subsubmenu');
+//       otherSubSubMenus.forEach(otherSubSubMenu => {
+//         if (otherSubSubMenu === lastVisitedSubSubMenu) {
+//           otherSubSubMenu.classList.remove('active');
+//         } 
+//       });
+//     }
+//   });
+// });
+
+
+
+
 
 function printTime() {
   var now = new Date();
@@ -96,7 +217,8 @@ function printTime() {
 
   var time = hours + ":" + minutes;
 
-  console.log(time); // Output to the console
+  //console.log(time); 
+  // Output to the console
   // You can also update the time dynamically on your webpage
   // by setting the value of an HTML element using JavaScript
   // For example:
