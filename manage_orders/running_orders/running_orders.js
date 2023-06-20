5a5a614ab46ffd926b8880f34efe32b19177a6d2
mayur-pixel6 <mayur@thinkoverit.com>
2023-06-20 12:06:32 +0530

    running order page css changes

---
+++ manage_orders/running_orders/running_orders.js
@@ -0,0 +1,16 @@
+"use strict"
+
+let tables = [
+    {tableNo: "T01", status:"occupied", availStateIcon:"", availStateText:"", occupiedStateIcon:"", occupiedStateSUBText:"E04A01", occupiedStatetext:"4,800", engagedStateIcon:"", engagedStatetext:"" },
+    {tableNo: "T01", status:"occupied", availStateIcon:"", availStateText:"", occupiedStateIcon:"", occupiedStateSUBText:"E04A01", occupiedStatetext:"4,800", engagedStateIcon:"", engagedStatetext:"" },
+    {tableNo: "T01", status:"occupied", availStateIcon:"", availStateText:"", occupiedStateIcon:"", occupiedStateSUBText:"E04A01", occupiedStatetext:"4,800", engagedStateIcon:"", engagedStatetext:"" },
+    {tableNo: "T01", status:"occupied", availStateIcon:"", availStateText:"", occupiedStateIcon:"", occupiedStateSUBText:"E04A01", occupiedStatetext:"4,800", engagedStateIcon:"", engagedStatetext:"" },
+    {tableNo: "T01", status:"occupied", availStateIcon:"", availStateText:"", occupiedStateIcon:"", occupiedStateSUBText:"E04A01", occupiedStatetext:"4,800", engagedStateIcon:"", engagedStatetext:"" },
+    {tableNo: "T01", status:"occupied", availStateIcon:"", availStateText:"", occupiedStateIcon:"", occupiedStateSUBText:"E04A01", occupiedStatetext:"4,800", engagedStateIcon:"", engagedStatetext:"" },
+    {tableNo: "T01", status:"occupied", availStateIcon:"", availStateText:"", occupiedStateIcon:"", occupiedStateSUBText:"E04A01", occupiedStatetext:"4,800", engagedStateIcon:"", engagedStatetext:"" },
+    {tableNo: "T01", status:"occupied", availStateIcon:"", availStateText:"", occupiedStateIcon:"", occupiedStateSUBText:"E04A01", occupiedStatetext:"4,800", engagedStateIcon:"", engagedStatetext:"" },
+    {tableNo: "T01", status:"occupied", availStateIcon:"", availStateText:"", occupiedStateIcon:"", occupiedStateSUBText:"E04A01", occupiedStatetext:"4,800", engagedStateIcon:"", engagedStatetext:"" },
+    {tableNo: "T01", status:"occupied", availStateIcon:"", availStateText:"", occupiedStateIcon:"", occupiedStateSUBText:"E04A01", occupiedStatetext:"4,800", engagedStateIcon:"", engagedStatetext:"" },
+    {tableNo: "T01", status:"occupied", availStateIcon:"", availStateText:"", occupiedStateIcon:"", occupiedStateSUBText:"E04A01", occupiedStatetext:"4,800", engagedStateIcon:"", engagedStatetext:"" }
+]
+
