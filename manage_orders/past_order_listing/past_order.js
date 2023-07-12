415c101b3dd0e6f1a81e19bd2c35e60c9ae3a81e
ashwinipixel6 <ashwini@pixel6.co>
2023-07-12 15:19:06 +0530

    past order view ashwini

---
+++ manage_orders/past_order_listing/past_order.js
@@ -0,0 +1,4 @@
+function myFunction1() {
+    document.querySelector(".running-orders-dash").classList.toggle("disp-none");
+    document.querySelector(".table-selected").classList.toggle("disp-none");
+}
