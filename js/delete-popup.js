eb081927f9912f8e0cfc563d95bfe0c4e027fe4d
mayur-pixel6 <mayur@thinkoverit.com>
2023-07-24 17:08:55 +0530

    running orders pop up

--- js/delete-popup.js
+++ js/delete-popup.js
@@ -1,5 +1,6 @@
 // let del = document.getElementById("delete-food-item");
 let popWin = document.querySelector(".global-container");
+let popWinMerge = document.querySelector(".global-container-merge");
 let cancelButton = document.querySelector(".cancel");
 
 // del.addEventListener("click", popIt);
@@ -16,8 +17,20 @@ function unPopIt(){
 }
 
 popWin.addEventListener("click",outWin);
+popWinMerge.addEventListener("click",outWinMerge);
 function outWin(e){
   if (e.target.id === "global"){
     unPopIt();
   }
+}
+function outWinMerge(e) {
+  console.log(e.target.id);
+  if (e.target.id === "globalMerge"){
+    popWinMerge.classList.toggle("pop");
+  }
+}
+
+function popItMerge() {
+  popWinMerge.classList.toggle("pop");
+  popWinMerge.scrollIntoView();
 }
