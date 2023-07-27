4190b37d72c7f03b756aee17a5aa8f9d74860dfb
parakh.pixel6 <parakh@pixel6.co>
2023-07-27 13:48:09 +0530

    Review Container

--- manage_orders/past_order_listing/past_order.js
+++ manage_orders/past_order_listing/past_order.js
@@ -9,3 +9,22 @@ function togglePastOrderFilterDivFun() {
     item.classList.toggle("disp-none");
   })
 }
+
+
+//My Review Nav
+const reviewNav = document.querySelector('.review-nav');
+const links = reviewNav.getElementsByTagName('a');
+
+reviewNav.addEventListener('click', function (event) {
+
+  // Check if the clicked element is an <a> tag
+  if (event.target.tagName === 'A') {
+    // Remove active class from all links
+    for (var i = 0; i < links.length; i++) {
+      links[i].classList.remove('active');
+    }
+
+    // Add active class to the clicked link
+    event.target.classList.add('active');
+  }
+});
